ce667265349c80121799de395d77564fd39fbe5a
dipesh <kcdipesh429@gmail.com>
2021-06-18 18:13:02 +0545

    feat: utility for bcrypt

---
+++ utils/bcrypt.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(pass *string) {
+	bytePass := []byte(*pass)
+	hPass, _ := bcrypt.GenerateFromPassword(bytePass, bcrypt.DefaultCost)
+	*pass = string(hPass)
+}
+
+func ComparePassword(dbPass, pass string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(dbPass), []byte(pass)) == nil
+}
+
+//GenerateToken -> generates token
+func GenerateToken(userid uint) string {
+	claims := jwt.MapClaims{
+		"exp":    time.Now().Add(time.Hour * 3).Unix(),
+		"iat":    time.Now().Unix(),
+		"userID": userid,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return t
+
+}
+
+//ValidateToken --> validate the given token
+func ValidateToken(token string) (*jwt.Token, error) {
+
+	//2nd arg function return secret key after checking if the signing method is HMAC and returned key is used by 'Parse' to decode the token)
+	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			//nil secret key
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+}
