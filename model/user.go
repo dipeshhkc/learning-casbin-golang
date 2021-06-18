34931bd2c2793d459006dedeca45a72ea2d9254a
dipesh <kcdipesh429@gmail.com>
2021-06-18 18:12:36 +0545

    feat:model for user entity

---
+++ model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "gorm.io/gorm"
+
+//User -> model for users table
+type User struct {
+	gorm.Model
+	Name     string `json:"name" `
+	Email    string `json:"email"  gorm:"unique"`
+	Password string `json:"password" `
+}
+
+//TableName --> Table for Product Model
+func (User) TableName() string {
+	return "users"
+}
